572d8042c677a770c177f2467e8d04d589c7390e
raulsl0 <raulsantileite@gmail.com>
2025-03-25 22:12:06 -0300

    calculadora

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 function calcular(){
-    alert("Funcionou")
+    //vamoscriar duas variaveis
+    //js as variaveis nao possuem tipo
+    let mascote, homenagem
+    mascote = document.getElementById("mascote").value
+    alert(mascote)
+    homenagem = document.getElementById("homenagem").value
+    alert(homenagem)
+    let soma = Number(mascote) + Number(homenagem)
+    alert (soma)
 }
