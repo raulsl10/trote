252d3965a7e737ba194e4ec57e695df822f2d549
raulsl0 <raulsantileite@gmail.com>
2025-03-27 20:38:40 -0300

    avulsos

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 function calcular(){
     //vamoscriar duas variaveis
     //js as variaveis nao possuem tipo
-    let mascote, homenagem
+    let mascote, homenagem, leite, kitAvulso, suplementoAvulso
     mascote = document.getElementById("mascote").value
-    alert(mascote)
     homenagem = document.getElementById("homenagem").value
-    alert(homenagem)
-    let soma = Number(mascote) + Number(homenagem)
-    alert (soma)
+    //recupera a quantidade de litros de leite
+    leite = document.getElementById("leite").value
+    kitAvulso = document.getElementById("kitAvulso").value
+    suplementoAvulso = document.getElementById("suplementoAvulso").value
+    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite))+(30 * Number(kitAvulso)) + (15 * Number(suplementoAvulso))
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
+}
+function mudarTexto(botao){
+    botao. textContent= "Calculado";
 }
