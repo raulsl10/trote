24f7496b0360a5e78b4cf19ab64a78bc0055964a
raulsl0 <raulsantileite@gmail.com>
2025-03-30 14:06:27 -0300

    calcauladora pronta

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,39 @@
 function calcular(){
     //vamoscriar duas variaveis
     //js as variaveis nao possuem tipo
-    let mascote, homenagem, leite, kitAvulso, suplementoAvulso
-    mascote = document.getElementById("mascote").value
-    homenagem = document.getElementById("homenagem").value
+    let mascote, homenagem, leite, kitAvulso, suplementoAvulso, arroz5Avulso, arroz1Avulso, feijao2Avulso, feijao1Avulso, macarraoAvulso, oleoAvulso, sangueAvulso, acaoSocial, fotoAs, fotoAcao;
+    mascote = document.getElementById("mascote").value;
+    homenagem = document.getElementById("homenagem").value;
     //recupera a quantidade de litros de leite
-    leite = document.getElementById("leite").value
-    kitAvulso = document.getElementById("kitAvulso").value
-    suplementoAvulso = document.getElementById("suplementoAvulso").value
-    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite))+(30 * Number(kitAvulso)) + (15 * Number(suplementoAvulso))
-    document.getElementById("soma").innerHTML = `A soma é ${soma}`
+    leite = document.getElementById("leite").value;
+    kitAvulso = document.getElementById("kitAvulso").value;
+    suplementoAvulso = document.getElementById("suplementoAvulso").value;
+    arroz5Avulso = document.getElementById("arroz5Avulso").value;
+    arroz1Avulso = document.getElementById("arroz1Avulso").value; 
+    feijao2Avulso = document.getElementById("feijao2Avulso").value;
+    feijao1Avulso = document.getElementById("feijao1Avulso").value;
+    macarraoAvulso = document.getElementById("macarraoAvulso").value;
+    oleoAvulso = document.getElementById("oleoAvulso").value;
+    sangueAvulso = document.getElementById("sangueAvulso").value;
+    acaoSocial = document.getElementById("acaoSocial").value;
+    fotoAs = document.getElementById("fotoAs").value;
+    fotoAcao = document.getElementById("fotoAcao").value;
+
+    let soma = Number(mascote) + Number(homenagem) + 
+               (2 * Number(leite))+
+               (30 * Number(kitAvulso)) + 
+               (15 * Number(suplementoAvulso)) + 
+               (5 * Number(arroz5Avulso)) + 
+               (1 * Number(arroz1Avulso)) + 
+               (2 * Number(feijao2Avulso)) + 
+               (1 * Number(feijao1Avulso)) + 
+               (0.5 * Number(macarraoAvulso)) + 
+               (1 * Number(oleoAvulso)) + 
+               (20 * Number(sangueAvulso)) + 
+               (10 * Number(acaoSocial)) + 
+               Number(fotoAs) + 
+               Number(fotoAcao);
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`;
 }
 function mudarTexto(botao){
     botao. textContent= "Calculado";
