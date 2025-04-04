2aee042c00688674b9ab547baf7cf07d57272c8d
raulsl0 <raulsantileite@gmail.com>
2025-04-03 21:16:24 -0300

    calcauladora daniel

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 function calcular(){
-    //vamoscriar duas variaveis
-    //js as variaveis nao possuem tipo
     let mascote, homenagem, leite, kit, suplemento, arroz5, arroz1, feijao2, feijao1, macarrao, oleo, sangue, acaoSocial, fotoAs, fotoAcao, soma, equipe
     mascote = Number (document.getElementById("mascote").value)
     homenagem = Number(document.getElementById("homenagem").value)
-    //recupera a quantidade de litros de leite
     leite = Number (document.getElementById("leite").value)
     kit =Number (document.getElementById("kit").value)
     suplemento = Number(document.getElementById("suplemento").value)
@@ -18,25 +15,27 @@ function calcular(){
     acaoSocial = Number(document.getElementById("acaoSocial").value)
     fotoAs = Number(document.getElementById("fotoAs").value)
     fotoAcao = Number(document.getElementById("fotoAcao").value)
-    
-    soma = mascote + homenagem + leite + (30 * (kit)) + suplemento + arroz5 + arroz1 
-    equipe == document.getElementById("equipe").value
-    if(equipe == "Laranja"){
-        if(kit >= 97){
-            soma = soma + 5000 + ((kit-97) * 30)
+  
+    soma = mascote + homenagem + leite + (30 * (kit)) + suplemento + arroz5 + arroz1 //corrigir soma
+
+    equipe = document.getElementById("equipe").value
+    if (equipe == "Laranja"){
+        if (kit >= 97 && suplemento >= 49) {
+            soma = soma + 5000 + ((kit-97) * 30) + ((suplemento - 49) * 15)
         }
-        else if( kit >=78){
-            soma = soma + 4000 + ((kit-78) * 30)
+        else if (kit >= 78 && suplemento >= 39){
+            soma = soma + 4000 + ((kit-78) * 30) + ((suplemento - 39) * 15)
         }
-        else if (kit >= 49){
-            soma = soma + 2500 + ((kit-49) * 30)
+        else if (kit >= 49 && suplemento >= 25){
+            soma = soma + 2500 + ((kit-49) * 30) + ((suplemento - 25) * 15)
         }
-        else if(kit >=19){
-            soma = soma + 1000 + ((kit-19) * 30)
+        else if (kit >= 19 && suplemento >= 10){
+            soma = soma + 1000 + ((kit-19) * 30) + ((suplemento - 10)* 15)
         }
     }
-    document.getElementById("soma").innerHTML = `A soma é ${soma}`;
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
 function mudarTexto(botao){
     botao. textContent= "Calculado";
 }
+
